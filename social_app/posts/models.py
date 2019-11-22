9d0923e6cf8aaef79e914b31661c0af490fd1342
brozzi24 <bmrozzi@student.ysu.edu>
2019-11-22 14:16:48 -0500

    Bug  fix with comments and datetime warning

--- social_app/posts/models.py
+++ social_app/posts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
-from django.utils import timezone
+
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=datetime.now)
 
     # To order the post by newest first
     class Meta:
@@ -20,7 +20,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=150)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         name = "{} {}".format(self.id, self.author)
